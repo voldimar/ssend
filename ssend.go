648cecfbfef2ad3b7515d19aea7903e60e25f61b
voldimar <root@gosp.cloud>
2021-05-25 13:46:21 +0300

    Example code from offsite

---
+++ ssend.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+)
+
+func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://dbe0d18ea06a4c7eac9f06ebf5dfaf45@log.gosp.cloud/2",
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+}
