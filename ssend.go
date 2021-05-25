67a113b3d4008bca36b7f1c20cddbd580f3fb02f
voldimar <root@gosp.cloud>
2021-05-25 17:17:49 +0300

    Add command line args to send.

--- ssend.go
+++ ssend.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/getsentry/sentry-go"
@@ -9,12 +11,13 @@ import (
 
 func main() {
 	err := sentry.Init(sentry.ClientOptions{
-		Dsn: "https://dbe0d18ea06a4c7eac9f06ebf5dfaf45@log.gosp.cloud/2",
+		Dsn:   "https://dbe0d18ea06a4c7eac9f06ebf5dfaf45:0a47b933c9c74bbab720537a139140d3@log.gosp.cloud/2",
+		Debug: true,
 	})
 	if err != nil {
 		log.Fatalf("sentry.Init: %s", err)
 	}
 	defer sentry.Flush(2 * time.Second)
 
-	sentry.CaptureMessage("It works!")
+	sentry.CaptureMessage(strings.Join(os.Args[1:], " "))
 }
